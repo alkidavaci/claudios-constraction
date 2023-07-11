57a23d7968a220b9275458210a68ce2cd4026b4c
Alkida Vaci <alkida.vaci@gmail.com>
2023-07-11 15:48:46 -0400

    about us page contents

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,27 +1,21 @@
-import profileImage from "../../assets/picture/kida1.jpg";
 import './style.css'
 
 function About() {
     return (
         <section>
-            <div className="about-header">
-                <h1 className="text-center">
-                    About Me
+            <div>
+                <h1>
+                    About Us
                 </h1>
             </div>
-            <div className="p-4" >
-                <div className="row d-flex justify-content-center align-items-center">
-                    <div className="col-md-2 p-5">
-                        <img src={profileImage}  alt="profile"  className="profile-image rounded img-fluid"/>
-                    </div>
-                    <div className="col-md-10 lh-lg p-5">
-                        <p className="about-text">I'm Alkida Vaci, an electronic engineering graduate from Albania currently living in New York City. I'm passionate about coding and I'm currently enrolled in a coding bootcamp to expand my knowledge and experience in the field.
-
-                        My background in engineering has equipped me with solid mathematical and logical skills, which have been useful in tackling complex coding problems. In addition, my strong communication and teamwork skills make me an asset to any coding team.
-
-                        My portfolio showcases my coding abilities and highlights my experience and skills in various projects such as web development and database management, demonstrating my versatility and expertise.
-
-                        As an entry-level programmer, I'm excited to continue learning and growing in the field. With my technical knowledge, strong work ethic, and passion for coding, I'm confident in my ability to become a successful programmer.
+            <div>
+                <div>
+                 
+                    <div>
+                        <p> 
+                        Irure est aliqua ut laboris esse consequat. Minim incididunt commodo laborum elit elit aliquip labore consequat ad ullamco. Id commodo sit elit aute ut nostrud ad voluptate minim minim sunt laborum. Elit ea cupidatat sit laboris tempor dolore fugiat ea ipsum non nulla exercitation qui. Et consequat irure officia incididunt culpa nostrud ad duis Lorem tempor aute commodo fugiat. Reprehenderit non pariatur sunt et duis cupidatat in laborum culpa nulla magna.
+                        
+                        Irure est aliqua ut laboris esse consequat. Minim incididunt commodo laborum elit elit aliquip labore consequat ad ullamco. Id commodo sit elit aute ut nostrud ad voluptate minim minim sunt laborum. Elit ea cupidatat sit laboris tempor dolore fugiat ea ipsum non nulla exercitation qui. Et consequat irure officia incididunt culpa nostrud ad duis Lorem tempor aute commodo fugiat. Reprehenderit non pariatur sunt et duis cupidatat in laborum culpa nulla magna.
                         </p>
                     </div>
                 </div>
